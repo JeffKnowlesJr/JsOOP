5cef14b06383c2eccde01e1fe7391de7d7ba0a27
Jefek <jeffrey.knowles@gmail.com>
2018-11-28 17:27:01 -0500

    Finished Project Implementation

--- cardDeck.js
+++ cardDeck.js
@@ -16,6 +16,12 @@ class Deck {
       }
     }
   }
+  reset() {
+//     console.log("Reset initialized");
+    let tempDeck = new Deck();
+//     console.log("tempDeck Constructed");
+    this.cards = tempDeck.cards;
+  }
   shuffle() {
     let newArray = [];
     // console.log("newArray " + newArray)
@@ -28,16 +34,25 @@ class Deck {
       newArray.push(this.cards[pick]);
       // console.log("newArray " + newArray);
       this.cards[pick] = this.cards[this.cards.length - 1];
-
       this.cards.length = this.cards.length -1;
     }
     this.cards = newArray;
   }
   spread() {
     // console.log("this.cards " + this.cards);
-    for (var i = 0; i < 52; i++) {
+    for (let i = 0; i < this.cards.length; i++) {
       this.cards[i].show();
     }
+  }
+  deal() {
+    let pick = Math.floor(Math.random() * this.cards.length );
+    let temp = this.cards[pick];
+    this.cards[pick] = this.cards[this.cards.length - 1];
+    this.cards.length = this.cards.length -1;
+    return temp;
+  }
+  deckSize() {
+    console.log("Deck has " + this.cards.length + " cards.");
   }
 }
 
@@ -51,7 +66,53 @@ class Card {
     console.log( this.key + " of " + this.suit);
   }
 }
+class Player {
+  constructor(name) {
+//     console.log("New player seated.")
+    this.name = name;
+//     console.log("Player name is " + this.name);
+    this.hand = [];
+  }
+  drawHand(deck) {
+    for (let i = 0; i < 5; i++) {
+      this.hand.push(deck.deal());
+    }
+  }
+  drawCard(deck) {
+    this.hand.push(deck.deal());
+  }
+  discard(finger) {
+    let temp = this.hand[finger];
+    this.cards[finger] = this.cards[this.cards.length - 1];
+    return temp;
+  }
+  showHand() {
+    console.log(this.name + ' says, Checkout my hand!');
+    for (let i = 0; i < this.hand.length; i++) {
+      this.hand[i].show();
+    }
+    console.log("_________________");
+  }
+}
 
-Deck1 = new Deck();
-Deck1.shuffle();
-Deck1.spread();
+var deck1 = new Deck();
+deck1.shuffle();
+deck1.spread();
+deck1.reset();
+deck1.spread();
+deck1.shuffle();
+deck1.shuffle();
+var jeff = new Player("Jeff");
+var pete = new Player("Pete");
+var zach = new Player("Zach");
+var jord = new Player("Jordan");
+jeff.drawHand(deck1);
+pete.drawHand(deck1);
+zach.drawHand(deck1);
+jord.drawHand(deck1);
+deck1.deckSize();
+jeff.showHand();
+pete.showHand();
+zach.showHand();
+jord.showHand();
+deck1.spread();
